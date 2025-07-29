69445f2cfa2650d19ba965319be74f6fbbbc854d
Totochesix <totochesix@gmail.com>
2025-07-29 23:24:26 +0200

    Update SlashConfig.js

--- SlashConfig.js
+++ SlashConfig.js
@@ -1,8 +1,11 @@
 const MODEL_MAP_ID = 2;             // ID de la map EventTools
 const MODEL_EVENT_NAME = "Caillou"; // Nom de l'événement modèle
-const CLONE_KEY = "pageup";         // Touche de clonage
+const CLONE_KEY = "pagedown";         // Touche de clonage
 const ANIMATION_ID = 121; // ID de l'animation
 const SLASH_IMAGE = "WeaponAnimationTransparent"; // image dans img/pictures/
 const SLASH_DURATION = 15;   // durée en frames
 const SLASH_RADIUS = 40;     // distance du balayage en pixels
 const SLASH_SCALE = 0.06;    // échelle du sprite
+const SLASH_COOLDOWN = 40;
+let slashcooldown = 0;
+SOUND_NAME_SLASH = "Slash1";
