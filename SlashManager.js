f7f712e7cfc44a2da22d87d7a8456436b0187dd0
Totochesix <totochesix@gmail.com>
2025-07-29 23:24:43 +0200

    Update SlashManager.js

--- SlashManager.js
+++ SlashManager.js
@@ -1,9 +1,10 @@
 const alias_update = Scene_Map.prototype.update;
   Scene_Map.prototype.update = function () {
     alias_update.call(this);
-    if (Input.isTriggered(CLONE_KEY)) {
+    if (Input.isTriggered(CLONE_KEY) && slashcooldown === 0) {
       const [x, y] = positionDevantLeJoueur();
-      cloneEventAt(x, y);
+      //cloneEventAt(x, y);
       spawnSlash();
     }
+    if (slashcooldown > 0) slashcooldown--;
   };
